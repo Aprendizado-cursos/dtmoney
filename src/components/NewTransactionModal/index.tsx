e6b4eb5aa01a40cccb7a07805735f7f8e600335d
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-08-31 18:45:47 -0300

    feat: :sparkles: Funcionamento dos Botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import React from "react";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const [type, setType] = React.useState("deposit");
+
     return (
         <Modal
             isOpen={isOpen}
@@ -25,14 +28,14 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input placeholder="Título"></input>
                 <input type="number" placeholder="Valor"></input>
                 <TransactionTypeContainer>
-                    <button type="button">
+                    <RadioBox type="button" onClick={() => setType("deposit")} isActive={type === "deposit"}>
                         <img src={incomeImg} alt="Entrada"></img>
                         <span>Entrada</span>
-                    </button>
-                    <button type="button">
+                    </RadioBox>
+                    <RadioBox type="button" onClick={() => setType("withdraw")} isActive={type === "withdraw"}>
                         <img src={outcomeImg} alt="Saída"></img>
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
                 <input placeholder="Categoria"></input>
                 <button type="submit">Cadastrar</button>
